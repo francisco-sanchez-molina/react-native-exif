56d1747970ff60877ac598edc89e848b978c0375
Francisco Sanchez <francisco.sanchez@bq.com>
2016-06-21 18:34:37 +0200

    unify model

--- index.js
+++ index.js
@@ -1,10 +1,35 @@
-import { NativeModules } from 'react-native';
+import {
+	Platform,
+	NativeModules } from 'react-native';
 
 var Exif = {}
 
+function unifyAndroid(exif){
+	var output = {} 
+
+	output.ImageWidth = parseInt(exif.ImageWidth)
+	output.ImageHeight = parseInt(exif.ImageHeight)
+	output.Orientation = parseInt(exif.Orientation)
+	return output
+}
+
+function unifyIOS(exif){
+	var output = {} 
+	output.ImageWidth = exif.PixelWidth
+	output.ImageHeight = exif.PixelHeight
+	output.Orientation = exif.Orientation
+	return output
+}
+
 Exif.getExif = function (uri) {
-  var path = uri.replace('file://', '')
-  return NativeModules.ReactNativeExif.getExif(path)
+	var path = uri.replace('file://', '')
+	return NativeModules.ReactNativeExif.getExif(path).then(result => {
+		if (Platform.OS === 'android') {
+			return unifyAndroid(result)
+		} else {
+			return unifyIOS(result)
+		}
+	})
 }
 
 
@@ -39,3 +64,8 @@ exifDic properties: {
 	        Orientation = 1;
 	    };
 	}*/
+
+/*
+android
+ {"WhiteBalance":"0","SubSecTime":null,"GPSDateStamp":null,"GPSLatitude":null,"GPSAltitude":null,"GPSAltitudeRef":null,"GPSLongitude":null,"SubSecTimeDigitized":null,"DateTime":"2016:06:18 21:07:03","GPSLongitudeRef":null,"FocalLength":"293/100","Flash":"0","ImageLength":"1836","DateTimeDigitized":"2016:06:18 21:07:03","GPSProcessingMethod":null,"ExposureTime":"0.030","Make":"SAMSUNG","FNumber":"2.400","GPSLatitudeRef":null,"GPSTimeStamp":null,"ImageWidth":"3264","SubSecTimeOriginal":null,"ISOSpeedRatings":"200","Model":"SM-G7105","Orientation":"6"}
+*/
