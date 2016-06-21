d910212493b30cc169e745a9c5c64b85df3cfea4
Francisco.Sanchez <francisco.sanchez@devialab.com>
2016-06-21 18:22:15 +0200

    exif on ios

--- index.js
+++ index.js
@@ -9,3 +9,33 @@ Exif.getExif = function (uri) {
 
 
 module.exports = Exif
+
+
+/*ios
+
+exifDic properties: {
+	    ColorModel = RGB;
+	    Depth = 8;
+	    Orientation = 1;
+	    PixelHeight = 240;
+	    PixelWidth = 320;
+	    ProfileName = "sRGB IEC61966-2.1";
+	    "{Exif}" =     {
+	        ColorSpace = 1;
+	        PixelXDimension = 320;
+	        PixelYDimension = 240;
+	    };
+	    "{JFIF}" =     {
+	        DensityUnit = 0;
+	        JFIFVersion =         (
+	            1,
+	            0,
+	            1
+	        );
+	        XDensity = 72;
+	        YDensity = 72;
+	    };
+	    "{TIFF}" =     {
+	        Orientation = 1;
+	    };
+	}*/
--- ios/ReactNativeExif/ReactNativeExif.m
+++ ios/ReactNativeExif/ReactNativeExif.m
@@ -2,6 +2,7 @@
 #import "RCTBridge.h"
 #import "RCTEventDispatcher.h"
 #import "RCTLog.h"
+@import Photos;
 
 @implementation ReactNativeExif
 
@@ -10,8 +11,26 @@ RCT_EXPORT_MODULE(ReactNativeExif)
 RCT_EXPORT_METHOD(getExif:(NSString *)path resolver:(RCTPromiseResolveBlock)resolve
                           rejecter:(RCTPromiseRejectBlock)reject) {
   
-    resolve(@"test  ios");
-  
+    @try {
+
+        UIImage * myImage = [UIImage imageWithContentsOfFile: path];
+        NSData* pngData =  UIImageJPEGRepresentation(myImage, 1.0);
+        
+        CGImageSourceRef mySourceRef = CGImageSourceCreateWithData((CFDataRef)pngData, NULL);
+        if (mySourceRef != NULL)
+        {
+            NSDictionary *myMetadata = (__bridge NSDictionary *)CGImageSourceCopyPropertiesAtIndex(mySourceRef,0,NULL);
+            resolve(myMetadata);
+        }
+        
+    }
+    @catch (NSException *exception) {
+        NSLog(@"%@", exception.reason);
+        NSError *error = [NSError errorWithDomain:@"world" code:200 userInfo:@"error"];
+        reject(@"fail", @"getExif", error);
+    }
+
+
 }
 
 @end
