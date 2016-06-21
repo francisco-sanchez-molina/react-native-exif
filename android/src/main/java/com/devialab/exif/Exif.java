510f0a3a5ab367f0c13cbca21eebb538b32bc248
Francisco Sanchez <francisco.sanchez@bq.com>
2016-06-21 13:15:59 +0200

    android exif working

--- android/src/main/java/com/devialab/exif/Exif.java
+++ android/src/main/java/com/devialab/exif/Exif.java
@@ -1,9 +1,46 @@
 package com.devialab.exif;
 
+import android.media.ExifInterface; 
+
 import com.facebook.react.bridge.*;
+
+import java.io.IOException;
+
+import com.facebook.react.bridge.Arguments;
+
+import com.facebook.react.bridge.WritableMap;
+
 
 public class Exif extends ReactContextBaseJavaModule  {
 
+    private static final String[] EXIF_ATTRIBUTES = new String[] {
+        ExifInterface.TAG_APERTURE,
+        ExifInterface.TAG_DATETIME,
+        ExifInterface.TAG_DATETIME_DIGITIZED,
+        ExifInterface.TAG_EXPOSURE_TIME,
+        ExifInterface.TAG_FLASH,
+        ExifInterface.TAG_FOCAL_LENGTH,
+        ExifInterface.TAG_GPS_ALTITUDE,
+        ExifInterface.TAG_GPS_ALTITUDE_REF,
+        ExifInterface.TAG_GPS_DATESTAMP,
+        ExifInterface.TAG_GPS_LATITUDE,
+        ExifInterface.TAG_GPS_LATITUDE_REF,
+        ExifInterface.TAG_GPS_LONGITUDE,
+        ExifInterface.TAG_GPS_LONGITUDE_REF,
+        ExifInterface.TAG_GPS_PROCESSING_METHOD,
+        ExifInterface.TAG_GPS_TIMESTAMP,
+        ExifInterface.TAG_IMAGE_LENGTH,
+        ExifInterface.TAG_IMAGE_WIDTH,
+        ExifInterface.TAG_ISO,
+        ExifInterface.TAG_MAKE,
+        ExifInterface.TAG_MODEL,
+        ExifInterface.TAG_ORIENTATION,
+        ExifInterface.TAG_SUBSEC_TIME,
+        ExifInterface.TAG_SUBSEC_TIME_DIG,
+        ExifInterface.TAG_SUBSEC_TIME_ORIG,
+        ExifInterface.TAG_WHITE_BALANCE
+    };
+    
     public Exif(ReactApplicationContext reactContext) {
         super(reactContext);
     }
@@ -15,7 +52,21 @@ public class Exif extends ReactContextBaseJavaModule  {
 
     @ReactMethod
     public void getExif( String uri, Callback successCallback,  Callback errorCallback) throws Exception {
-        successCallback.invoke("android test " + uri);
+        ExifInterface exif; 
+        try { 
+            exif = new ExifInterface(uri); 
+        } catch (IOException e) { 
+            errorCallback.invoke(e.toString());
+            return;
+        } 
+
+        WritableMap exifMap = Arguments.createMap();
+        for (String attribute : EXIF_ATTRIBUTES) {
+            String value = exif.getAttribute(attribute);
+            exifMap.putString(attribute, value);
+        }
+
+        successCallback.invoke(exifMap);
     }
 
 }
--- index.js
+++ index.js
@@ -3,7 +3,8 @@ import { NativeModules } from 'react-native';
 var Exif = {}
 
 Exif.getExif = function (uri, success, error) {
-  NativeModules.ReactNativeExif.getExif(uri, success, error)
+  var path = uri.replace('file://', '')
+  NativeModules.ReactNativeExif.getExif(path, success, error)
   return uri
 }
 
