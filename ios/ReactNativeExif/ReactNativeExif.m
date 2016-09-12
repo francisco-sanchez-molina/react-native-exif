60c117a7ebf82bb09916d87c4e10eb5edea82780
Francisco.Sanchez <francisco.sanchez@devialab.com>
2016-09-12 08:58:44 +0200

    add ios support camera roll

--- ios/ReactNativeExif/ReactNativeExif.m
+++ ios/ReactNativeExif/ReactNativeExif.m
@@ -2,6 +2,8 @@
 #import "RCTBridge.h"
 #import "RCTEventDispatcher.h"
 #import "RCTLog.h"
+#import <AssetsLibrary/AssetsLibrary.h>
+
 @import Photos;
 
 @implementation ReactNativeExif
@@ -11,16 +13,38 @@ RCT_EXPORT_MODULE(ReactNativeExif)
 RCT_EXPORT_METHOD(getExif:(NSString *)path resolver:(RCTPromiseResolveBlock)resolve
                           rejecter:(RCTPromiseRejectBlock)reject) {
   
+    
     @try {
 
-        UIImage * myImage = [UIImage imageWithContentsOfFile: path];
-        NSData* pngData =  UIImageJPEGRepresentation(myImage, 1.0);
+        if([path hasPrefix:@"assets-library"]) {
+            
+            ALAssetsLibraryAssetForURLResultBlock resultblock = ^(ALAsset *myasset)
+            {
+                
+                NSDictionary *myMetadata = (__bridge NSDictionary *)[[myasset defaultRepresentation] metadata];
+                resolve(myMetadata);
+                
+            };
+            
+            ALAssetsLibrary* assetslibrary = [[[ALAssetsLibrary alloc] init] autorelease];
+            NSURL *url = [NSURL URLWithString:[path stringByAddingPercentEscapesUsingEncoding:NSUTF8StringEncoding]];
+            [assetslibrary assetForURL:url
+                           resultBlock:resultblock
+                          failureBlock:^(NSError *error) {
+                              NSLog(@"error couldn't get photo");
+                          }];
+            
+        } else {
         
-        CGImageSourceRef mySourceRef = CGImageSourceCreateWithData((CFDataRef)pngData, NULL);
-        if (mySourceRef != NULL)
-        {
-            NSDictionary *myMetadata = (__bridge NSDictionary *)CGImageSourceCopyPropertiesAtIndex(mySourceRef,0,NULL);
-            resolve(myMetadata);
+            UIImage * myImage = [UIImage imageWithContentsOfFile: path];
+            NSData* pngData =  UIImageJPEGRepresentation(myImage, 1.0);
+        
+            CGImageSourceRef mySourceRef = CGImageSourceCreateWithData((CFDataRef)pngData, NULL);
+            if (mySourceRef != NULL)
+            {
+                NSDictionary *myMetadata = (__bridge NSDictionary *)CGImageSourceCopyPropertiesAtIndex(mySourceRef,0,NULL);
+                resolve(myMetadata);
+            }
         }
         
     }
